ac34f4ad6a582c8968a83cb477620357e902eaa7
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-08-10 16:49:24 +0900

    Use template for DLex

--- source/dlex/DLex.d
+++ source/dlex/DLex.d
@@ -8,73 +8,77 @@ import dlex.Position;
 import dlex.MatchResult;
 import dlex.Rule;
 
-
 enum Type {
     Int
 }
 
-struct RuleT {
-    Type type;
-    Rule rule;
-}
-class DLex {
-    public:
 
-	RuleT[] rules;
-	this (RuleT[] rules) {
-	    this.rules = rules;
-	}
-
-	auto Lex(dstring source) {
-	    Position pos;
-	    LexResult[] results = [];
-
-	    while (!pos.end(source)) {
-		Tuple!(MatchResult, Position, Type)[] rs = [];
-		foreach (rule; rules) {
-		    auto savePos = pos;
-		    MatchResult r = rule.rule.match(source, savePos);
-		    if (r) {
-			rs ~= tuple(r, savePos, rule.type); 
-		    }
-		}
-		if (rs.length == 0) {
-		    break;
-		}
-
-		// 最長一致なので最長のMatchResultを採用する
-		// 同じ長さならより早く見つけた（先に登録した）方を優先する
-		auto result = rs.reduce!((a,b) => (
-			(a[0].str.length >= b[0].str.length) ? a : b
-		));
-
-		results ~= new LexResult(result[2], result[0].str, pos);
-		pos = result[1];
+template DLex(Type) {
+    class DLex {
+	public:
+	    struct RuleT {
+		Type type;
+		Rule rule;
 	    }
 
-	    return results;
-	}
-}
-class LexResult {
-    public:
-	Type type;
-	dstring str;
-	Position pos;
+	    RuleT[] rules;
+	    
+	    void Rules(RuleT[] rules) {
+		this.rules = rules;
+	    }
 
-	this (Type type, dstring str, Position pos) {
-	    this.type = type;
-	    this.str = str;
-	    this.pos = pos;
-	}
+	    auto Lex(dstring source) {
+		Position pos;
+		LexResult[] results = [];
+
+		while (!pos.end(source)) {
+		    Tuple!(MatchResult, Position, Type)[] rs = [];
+		    foreach (rule; rules) {
+			auto savePos = pos;
+			MatchResult r = rule.rule.match(source, savePos);
+			if (r) {
+			    rs ~= tuple(r, savePos, rule.type); 
+			}
+		    }
+		    if (rs.length == 0) {
+			break;
+		    }
+
+		    // 最長一致なので最長のMatchResultを採用する
+		    // 同じ長さならより早く見つけた（先に登録した）方を優先する
+		    auto result = rs.reduce!((a,b) => (
+				(a[0].str.length >= b[0].str.length) ? a : b
+				));
+
+		    results ~= new LexResult(result[2], result[0].str, pos);
+		    pos = result[1];
+		}
+
+		return results;
+	    }
+    }
+    class LexResult {
+	public:
+	    Type type;
+	    dstring str;
+	    Position pos;
+
+	    this (Type type, dstring str, Position pos) {
+		this.type = type;
+		this.str = str;
+		this.pos = pos;
+	    }
+    }
 }
 
 unittest {
     import std.uni;
 
-    auto dlex = new DLex([
-	    RuleT(Type.Int, Char('I') + Pred(&isAlpha).Repeat)
+    auto dlex = new DLex!(Type);
+    dlex.Rules([
+	    dlex.RuleT(Type.Int, Char('I') + Pred(&isAlpha).Repeat)
     ]);
-    LexResult[] res = dlex.Lex("Int");
+    auto res = dlex.Lex("Int");
 
     assert (res.length == 1);
     assert (res[0].type == Type.Int);
